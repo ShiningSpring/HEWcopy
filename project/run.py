61f61bcfd18d093c8e36aca6e63cc4ee7c755edc
ShiningSpring <harutoto1101@gmail.com>
2025-02-04 11:30:34 +0900

    7

--- project/run.py
+++ project/run.py
@@ -1,5 +1,7 @@
 from app import create_app
+import os
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render が提供するポートを使用
+    app.run(host="0.0.0.0", port=port)
